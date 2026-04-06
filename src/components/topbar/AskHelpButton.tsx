f65e608381a32cd203c61f01d10ec3416edac4a2
sara lee <saragatinha@gmail.com>
2026-04-06 20:18:57 -0300

    feat(AskHelpButton): button now applies penalty

--- src/components/topbar/AskHelpButton.tsx
+++ src/components/topbar/AskHelpButton.tsx
@@ -1,6 +1,12 @@
+import { useTimerContext } from "../../context/TimerContext"
+
 export default function AskHelpButton() {
+  const { applyPenalty } = useTimerContext()
   return (
-    <button className="bg-blue-500 rounded-2xl text-white px-20 text-xl font-bold">
+    <button 
+      className="bg-blue-500 rounded-2xl text-white px-20 text-xl font-bold"
+      onClick={() => applyPenalty(5 * 60)}
+    >
       Solicitar Auxilio a Central
     </button>
   )
