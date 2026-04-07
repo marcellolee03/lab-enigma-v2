fa7f70b13a11eee35b2db51135ceb80361095922
sara lee <saragatinha@gmail.com>
2026-04-07 20:27:13 -0300

    feat(DigitContainer): added animation for when number is revealed

--- src/components/bottombar/pieces/DigitContainer.tsx
+++ src/components/bottombar/pieces/DigitContainer.tsx
@@ -6,7 +6,7 @@ export default function DigitContainer({digit}: DigitContainerProps){
   console.log("alou")
   return (
     <div className="rounded-lg text-4xl min-w-12 min-h-12 bg-slate-300 px-2 py-1 text-slate-900 font-semibold text-center">
-      <p>{digit}</p>
+      <p className={`transition-all duration-300 ${digit ? 'opacity-100 scale-100' : 'opacity-0 scale-75'}`}>{digit}</p>
     </div>
   )
 }
