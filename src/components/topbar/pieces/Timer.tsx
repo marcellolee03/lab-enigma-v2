ac119203b9b86e461ea3ff6caf04cb8faafa408d
sara lee <saragatinha@gmail.com>
2026-04-07 18:55:55 -0300

    feat(Timer): shaking animation for when user fails

--- src/components/topbar/pieces/Timer.tsx
+++ src/components/topbar/pieces/Timer.tsx
@@ -2,7 +2,7 @@ import { useTimerContext } from "../../../context/TimerContext";
 import { ClockIcon } from "../../icons";
 
 export default function Timer() {
-  const { getRemainingTime } = useTimerContext();
+  const { getRemainingTime, getIsPenalized } = useTimerContext();
   const remainingSeconds = getRemainingTime();
   
   function formatTime(remainingSeconds: number) {
@@ -15,13 +15,19 @@ export default function Timer() {
   
   const formattedTime = formatTime(remainingSeconds);
   
+  const remainingTimeClasses = `text-4xl duration-200 ${
+    getIsPenalized()
+      ? "text-red-200 animate-shake"
+      : ""
+    }`
+  
   return (
     <div className="rounded-2xl shadow-lg col-span-2 p-5 bg-linear-to-br from-red-600 to-red-900 text-slate-100 font-bold text-center grid grid-cols-1 gap-1">
       <div className="flex items-center justify-center gap-1">
         <ClockIcon />
         <p className="text-sm">Tempo Restante:</p>
       </div>
-      <p className="text-4xl">{formattedTime}</p>
+      <p className={remainingTimeClasses}>{formattedTime}</p>
     </div>
   )
 }
