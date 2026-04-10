fbc1d6621df6f3384bdc285da1cf144a19f04ee3
sara lee <saragatinha@gmail.com>
2026-04-10 14:59:17 -0300

    feat: added animations for topbar and bottombar

--- src/components/bottombar/Bottombar.tsx
+++ src/components/bottombar/Bottombar.tsx
@@ -3,7 +3,7 @@ import RevealedCodeContainer from "./pieces/RevealedCodeContainer";
 export default function Bottombar() {
   
   return (
-    <div className="fixed bottom-0 border-t-2 border-(--main-green) justify-center inset-x-0 z-50 mx-130 flex items-center rounded-t-2xl shadow-2xl bg-slate-100 px-10 py-4 gap-5">
+    <div className="fixed bottom-0 border-t-2 border-(--main-green) justify-center inset-x-0 z-50 mx-130 flex items-center rounded-t-2xl shadow-2xl bg-slate-100 px-10 py-4 gap-5 animate-fade-up [animation-delay:1100ms]">
       <RevealedCodeContainer />
     </div>
   )
--- src/components/topbar/Topbar.tsx
+++ src/components/topbar/Topbar.tsx
@@ -5,7 +5,7 @@ import Timer from "./pieces/Timer";
 export default function Topbar() {
   return (
     <>
-      <div className="fixed top-0 inset-x-0 z-50 mx-50 rounded-b-2xl shadow-2xl bg-slate-100 px-12 pb-5 pt-3 gap-5 border-b-3 border-(--main-green)">
+      <div className="fixed top-0 inset-x-0 z-50 mx-50 rounded-b-2xl shadow-2xl bg-slate-100 px-12 pb-5 pt-3 gap-5 border-b-3 border-(--main-green) animate-fade-down [animation-delay:200ms]">
         <Header />
         <div className="grid grid-cols-3 gap-5 px-40 pt-2">
           <Timer />
